290a34b65dc466efb9631b56b725f1fa49380b13
Opt1mus26 <pab1o@mail.ru>
2021-10-30 14:48:34 +0300

    Complete

--- loginAPP/ViewController.swift
+++ loginAPP/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var loginOutlet: UITextField!
+    @IBOutlet var passwordOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginOutlet.delegate = self
+        loginOutlet.returnKeyType = .next
+        
+        passwordOutlet.delegate = self
+        passwordOutlet.returnKeyType = .done
+        
+        passwordOutlet.isEnabled = true
+        passwordOutlet.enablesReturnKeyAutomatically = true
     }
-
-
+    
+    @IBAction func forgotUserNameAction() {
+        showAlert(title: "Oops!", message: "Your name is User")
+    }
+    
+    @IBAction func forgotPasswordAction() {
+        showAlert(title: "Oops!", message: "Your password is Password")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? WelcomeViewController else { return }
+        dvc.login = loginOutlet.text
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        loginOutlet.text = ""
+        passwordOutlet.text = ""
+    }
+    
+    @IBAction func logInButton() {
+        let loginTF = "User"
+        let passwordTF = "Password"
+        guard loginOutlet.text == loginTF else {
+            showAlert(title: "Invalid login or password", message: "Please, enter correct login and password"); return }
+        guard passwordOutlet.text == passwordTF else {
+            showAlert(title: "Invalid login or password", message: "Please, enter correct login and password"); return }
+    }
 }
 
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAktion = UIAlertAction(title: "OK", style: .default) { _ in self.passwordOutlet.text = ""
+        }
+        alert.addAction(okAktion)
+        present(alert, animated: true)
+    }
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == loginOutlet {
+            passwordOutlet.resignFirstResponder()
+        } else {
+            logInButton()
+            performSegue(withIdentifier: "dvc", sender: nil)
+        }
+        return true
+    }
+}
